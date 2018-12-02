c3706e692d0f0f15f9209e4d83fdbe13be86ad98
Andrii Zaburiannyi <andrewzabur@gmail.com>
2018-12-02 19:06:23 +0200

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '445697375957067',
+        clientSecret: 'ac6e4183618b546f4993e51efd382e48'
+    }
 }
